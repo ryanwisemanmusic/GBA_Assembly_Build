aeec6be392fee966d7f4ee5856ee19354dd99a96
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2025-04-24 13:15:18 -0700

    EntryPoint
    This is the bare minimum required to work with the hardware directly, and hence, is the minimum required for our application.
    
    Successfully, we have a working build for my M2 Mac. This will allow me to write my first simplistic game, which has more to do with me wanting to work with Assembly code directly for graphics (since there aren't many convenient ways outside of stuff like this)

--- src/main.asm
+++ src/main.asm
@@ -1,3 +1,4 @@
+INCLUDE "hardware.inc"
 
 SECTION "Header", ROM0[$100]
     jp EntryPoint
@@ -5,6 +6,25 @@ SECTION "Header", ROM0[$100]
 
 SECTION "Entry Point", ROM0[$150]
 EntryPoint:
-    ; Your code here
-    ret
+    ;This disables interrupts
+    di 
 
+    ;FFFE is the very top of the stack, we load this into the stack pointer
+    ld sp, $FFFE
+    
+    ;We then call our first loop
+    call WaitVBlank
+
+    ld a, [$ff00+c]
+    ldh [$ff00], a
+
+.loop:
+    halt
+    nop 
+    jr .loop
+
+WaitVBlank:
+    ldh a, [rLY]
+    cp 144
+    jr c, WaitVBlank
+    ret
